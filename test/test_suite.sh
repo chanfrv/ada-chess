f1ae169a7c7a28b115b07c655ea1b39578a3145c
Victor Chanfrault <victor.chanfrault@outlook.fr>
2021-02-06 14:46:55 +0100

    better test suite

--- test/test_suite.sh
+++ test/test_suite.sh
@@ -3,21 +3,16 @@
 
 test_dir="$(dirname $0)/../obj"
 
-case $1 in
-    "hlr")
-        test_prefix="hlr" ;;
-    "llr")
-        test_prefix="llr" ;;
-    ""|"*")
-        echo "Needs 'hlr' or 'llr' as argument"
-        exit 1 ;;
-esac
+if [ "$#" -ne 1 ] || ([ "$1" != "hlr" ] && [ "$1" != "llr" ]); then
+    echo "Needs 'hlr' or 'llr' as argument"
+    exit 1
+fi
 
-for test in "$test_dir/$test_prefix"*_pass "$test_dir/$test_prefix"*_fail; do
+for test in "$test_dir/$1"*_{pass,fail}; do
     ./$test 2>&1 > /dev/null
     if [ $? -eq 0 ]; then
-        echo -e "$test... \e[0;92mpass\e[0m"
+        echo -e "$(basename $test)... \e[0;92mpass\e[0m"
     else
-        echo -e "$test... \e[0;91mfail\e[0m"
+        echo -e "$(basename $test)... \e[0;91mfail\e[0m"
     fi
 done
